47ec2404081ea6dac449aaae1cc20806c351caea
espoirbaraka <esbarakabigega@example.com>
2023-02-07 09:05:59 +0100

    router

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,7 +1,19 @@
 <script>
 export default {
   name : 'AboutMe',
-  props :['nom','postnom']
+  props :['nom','postnom'],
+  beforeCreate() {console.log('Je ne suis pas encore cree')
+  },
+  created() {console.log('Je suis de ja cree')
+  },
+  beforeMount() {console.log('Je vais bientot etre monte')
+  },
+  mounted() {console.log('Je suis deja monte')
+  },
+  beforeDestroy() {console.log('Je vais etre detruit')
+  },
+  destroyed() {console.log('Je suis deja detruit')
+  }
 }
 </script>
 <template>
