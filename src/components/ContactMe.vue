262842296dc28821ca1c2d4c88a8fc64ebf448d5
espoirbaraka <esbarakabigega@example.com>
2023-02-07 10:21:27 +0100

    router

---
+++ src/components/ContactMe.vue
@@ -0,0 +1,29 @@
+<script>
+  export default {
+    name : 'ContactMe',
+    props: {
+      tel : {
+        type: String,
+        default : '+243977553723'
+      },
+      email :{
+        type: String
+      }
+    }
+  }
+</script>
+<template>
+  <div>
+    <div class="contact">
+      Mon numero est {{tel}}
+    </div>
+    <div class="email">
+      Mon email est {{email}}
+    </div>
+  </div>
+</template>
+<style>
+  .contact{
+    color: #42b983;
+  }
+</style>
